dec8febaa93271402631eaae0cda2e49d40a5fdd
Yuri Odilon <moura.n.yuri@gmail.com>
2023-12-04 22:23:45 -0300

    arquivo js accordion refatorado

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import ScrollSuave from "./modules/scrollSuave.js";
 import initAnimarScroll from "./modules/scrollAnime.js";
 import initTabNav from "./modules/tabNav.js";
-import initAccordion from "./modules/accordion.js";
+import Accordion from "./modules/accordion.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/toolTip.js";
 import initDropdownMenu from "./modules/dropdownMenu.js";
@@ -13,9 +13,11 @@ import initFetchBitcoin from "./modules/fetchBitcoin.js";
 const scrollsuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollsuave.init();
 
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
 initAnimarScroll();
 initTabNav();
-initAccordion();
 initModal();
 initTooltip();
 initDropdownMenu();
