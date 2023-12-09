8638c0e18d9c87fe980fc38a14af7c4f6c103263
Yuri Odilon <moura.n.yuri@gmail.com>
2023-12-08 23:29:50 -0300

    Ajuste no animar ao scroll

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -1,9 +1,9 @@
-import debounce from './debounce.js';
+import debounce from "./debounce.js";
 
 export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
-    this.windowMetade = window.innerHeight * 0.6;
+    this.windowMetade = window.innerHeight * 0.4;
 
     this.checkDistance = debounce(this.checkDistance.bind(this), 50);
   }
@@ -25,9 +25,9 @@ export default class ScrollAnima {
   checkDistance() {
     this.distance.forEach((item) => {
       if (window.pageYOffset > item.offset) {
-        item.element.classList.add('ativo');
-      } else if (item.element.classList.contains('ativo')) {
-        item.element.classList.remove('ativo');
+        item.element.classList.add("ativo");
+      } else if (item.element.classList.contains("ativo")) {
+        item.element.classList.remove("ativo");
       }
     });
   }
@@ -36,13 +36,13 @@ export default class ScrollAnima {
     if (this.sections.length) {
       this.getDistance();
       this.checkDistance();
-      window.addEventListener('scroll', this.checkDistance);
+      window.addEventListener("scroll", this.checkDistance);
     }
     return this;
   }
 
   // Remove o event de scroll
   stop() {
-    window.removeEventListener('scroll', this.checkDistance);
+    window.removeEventListener("scroll", this.checkDistance);
   }
 }
